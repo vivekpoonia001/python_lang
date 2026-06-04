7ca5fdf368a5382db9c39146fd7ace3cd2c80d8c
Vivek Poonia <vivekpoonia2005@gmail.com>
2026-06-05 01:36:02 +0530

    list querries

--- self_study.py
+++ self_study.py
@@ -36,23 +36,64 @@
 # else:
     # print("minimum balanace not found")
 
-percentage = int(input("enter your percentage "))
-if percentage >75:
-    f_income = int(input("enter your family_income"))
-    if f_income <300000:
-        print("scholar ship granted")
-    else:
-        print("scholarship denied")
-else:
-    print("not eligble")
+# percentage = int(input("enter your percentage "))
+# if percentage >75:
+#     f_income = int(input("enter your family_income"))
+#     if f_income <300000:
+#         print("scholar ship granted")
+#     else:
+#         print("scholarship denied")
+# else:
+#     print("not eligble")
 
-username = int(input("enter your username"))
-if username == "yes":
-    password = int(input("enter your password"))
-    if password == "correct":
-        print("login succsefull")
-    else:
-        print("incoreect passowrd")
-else:
-    print("else user not found")
-print("new repositry is made")
+# username = int(input("enter your username"))
+# if username == "yes":
+#     password = int(input("enter your password"))
+#     if password == "correct":
+#         print("login succsefull")
+#     else:
+#         print("incoreect passowrd")
+# else:
+#     print("else user not found")
+# print("new repositry is made")
+# l1 = ["Ashish", "Jogender","Nishant", "Harkarn","Manjot"]
+# num_1 = input("enter a friend name: ")
+# l1.insert(2,num_1)
+# mst_friend = input("enter your most important friend: ")
+# pos = int(input("enter postion of that friend: "))
+# l1.insert(pos,mst_friend)
+# print(l1)
+# l1 = [1,10,100,3,6,8]
+# l1.insert(3,59)
+# print(l1)
+# l1.append(5)
+# print(l1)
+# print(len(l1))
+# l1 = ["Ankit","jog","NIs","Vivek","Jaat","San"]
+# for i in l1:
+#     if len(i)<4:
+#         print(i)
+#another method 
+
+# num_1 = []
+# l1 = ["Ankit","jog","NIs","Vivek","Jaat","San"]
+# for i in l1:
+#     if len(i)<4:
+#         num_1.append(i)
+# print(num_1)
+
+# num_1 = range(20)
+# result=[]
+# for i in num_1:
+#     if i %2==0:
+#         result.append("even")
+#     else:
+#         result.append("odd")
+# print(result)
+# sum of list 
+l1 = [1,2,4,667,7,866,788,56]
+sum =0
+for i in l1: 
+    sum+=i
+print(sum)
+
