bf29d8495c5f1dad7d7e7b70230709ef60f009e4
Vivek Poonia <vivekpoonia2005@gmail.com>
2026-06-14 20:41:22 +0530

    n

--- quiz_applications.py
+++ quiz_applications.py
@@ -1,7 +1,6 @@
 print("Welcome to the Quiz")
 quiz = []
 def add_list():
-    while True:
         print("enter list of your questions")
         num = int(input("enter your range of questions"))
         for i in range(1, num+1):
@@ -12,8 +11,41 @@ def add_list():
             quiz.append(list_1)
             print(quiz)
 def list_quiz():
-    while True:
+        for index , list_1 in enumerate(quiz, start = 1):
+            print(f"{index} {list_1['question']} {list_1['option']}{list_1['answer']}")
+
+
+def update_list():
+    for i , q in enumerate(quiz, start = 1):
+          print(f"{i}.{q['quiz']}")
+    index = print(input("enter the index question for update"))
+    if 1<= index<=len(quiz):
+          quiz[index-1]('qst') = input("enter new question")
+          quiz[index-1]('opt') = input("enter new options").split(",")
+          quiz[index-1]('ans') = input("enter your new answer")
+          print("updated sucesfully")
+          print(quiz)
+    else:
+          print("invalid index")
+
+def delete_list():
+      if not quiz:
+            print("there is now such index")
+      else:
+         for index ,q in enumerate(quiz, start =1):
+               print(f"{index}. {q['quiz']}")
+         index = print(input("enter your question to delete"))
+         if 1<=index<=len(quiz):
+               deleted_question = quiz[index-1]['question']
+               del quiz[index-1]
+               
+
         
+               
+        
+
+
+
     
 
 
