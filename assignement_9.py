6a8534a22dc653ce004b34200b4a6391935eff6f
Vivek Poonia <vivekpoonia2005@gmail.com>
2026-06-17 11:55:43 +0530

    assignement 10

--- assignement_9.py
+++ assignement_9.py
@@ -1,3 +1,4 @@
+
 # l_f = ["apple\n", "watermelon\n", "pineapple\n"]
 # with open("fruits.txt", "w") as file:
 #     file.writelines(l_f)
@@ -23,33 +24,3 @@
 # with open ("fruits.txt", "r")as file:
 #    for i in file:
 #       print(i.strip())
-
-
-#question 5
-# try :
-#    num_1 = 10
-#    num_2 = int(input("enter number "))
-#    result = num_1/num_2
-# except ValueError:
-#    print("user entered a non number")
-# except ZeroDivisionError:
-#    print("10 cant be divide by zero")
-# else:
-#    print(result)
-# finally:
-#    print("divsion attempt finished")
-
-
-#question 6
-# my_colors = ["red", "blue", "green"]
-# student_info = {"name": "John", "grade": "A"}
-# try:
-#    print(my_colors[5])
-# except Exception as e :
-#     print("index error",e)
-
-# try:
-#    print(student_info["age"])
-# except Exception as e :
-#    print("enter a valid key",e)
-   
